f21d619d60af4c442099a5e02c807cd5cef58126
Cass07 <kjh95828@naver.com>
2024-10-17 00:35:07 +0900

    [Chore] Spring Boot 애플리케이션 생성
    Spring Boot 빈 애플리케이션 생성
    - JPA, DB-Driver 추가
    - JPA 초기화를 위해 application.yaml에 DB url, user, password 설정 추가

---
+++ src/test/java/dev/cass/knorda/KnordaApplicationTests.java
@@ -0,0 +1,12 @@
+package dev.cass.knorda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class KnordaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
